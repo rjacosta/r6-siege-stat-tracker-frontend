7dd167d1a39ae8e022cc667c0cdf061fe8d2786b
Romel <romeljacosta@gmail.com>
2020-08-17 21:11:59 -0700

    working on animation timing

--- src/components/UserCard/Stats.js
+++ src/components/UserCard/Stats.js
@@ -6,29 +6,51 @@ const Stats = ({stats}) => {
     const toggleShowStats = () => {
         setShowStats(!showStats);
     }
+    const [hideAnimationOccuring, setHideAnimationOccuring] = useState(false);
 
     const [containerClass, setContainerClass] = useState("opInfoContainerShow")
 
-    const toggleContainerClass = (toggleBorder) => {
-        if (!toggleBorder) {
+    const toggleContainerClass = (inContainer, toggleStats) => {
+        if (toggleStats) {
             setContainerClass(showStats ? 
             "opInfoContainerHide" : "opInfoContainerShow");
             toggleShowStats();
-        } else {
-            setContainerClass("opInfoContainerHideEnter")
+        } else if (!showStats) {
+            setTimeout(() => {
+                setHideAnimationOccuring(false);
+                if (inContainer) {
+                    setContainerClass("opInfoContainerHideEnter");
+                } else {
+                    setContainerClass("opInfoContainerHideOut");
+                }
+            }, hideAnimationOccuring ? 1000 : 0)            
         }
     }
-
+/*console.log(document.getElementById("container"))
+    const container = document.getElementById("container");
+    container.addEventListener("animationstart",
+    (e) => {
+        if (container.classList.contains("opInfoContainerHide")) {
+            setHideAnimationOccuring(true);
+        }
+    }, false)
+    container.addEventListener("animationend", 
+    (e) => {
+        if (container.classList.contains("opInfoContainerHide")) {
+            setHideAnimationOccuring(false);
+        }
+    }, false);*/
     return (
-            <div className={stats.ctu === undefined ? "" : containerClass }
-                onMouseEnter={() => {toggleContainerClass(true)}}
-                onMouseExit={() => {toggleContainerClass(true)}}
-                onMouseDown={() => {toggleContainerClass(false)}}
+            <div id="container" className={stats.ctu === undefined ? "" : containerClass }
+                onMouseEnter={() => {toggleContainerClass(true, false)}}
+                onMouseOut={() => {toggleContainerClass(false, false)}}
+                onMouseDown={() => {toggleContainerClass(false, true)}}
                 >
                 {stats.ctu === undefined ? null : 
                     <div className={"caption " + (showStats ? "opDataTitleShow" : "opDataTitleHide")}>Operator Stats</div>}
                 <div className={stats.ctu === undefined ? "opDataGrid": 
                     (showStats ? "opDataGridShow" : "opDataGridHide")}>
+
                     <div className="caption">W/L: {stats.wl}</div>
                     <div className="caption">Wins: {stats.wins}</div>
                     <div className="caption">Losses: {stats.losses}</div>
@@ -36,6 +58,7 @@ const Stats = ({stats}) => {
                     <div className="caption">Kills: {stats.kills}</div>
                     <div className="caption">Deaths: {stats.deaths}</div>
                 </div>
+
             </div>
     )
 }
