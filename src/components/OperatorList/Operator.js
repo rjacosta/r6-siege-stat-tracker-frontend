85cce4c67243a963c4353a16f5ab6f1a5fc2249e
Romel <romeljacosta@gmail.com>
2020-08-24 15:19:37 -0700

    working on op list animation

--- src/components/OperatorList/Operator.js
+++ src/components/OperatorList/Operator.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Stats from "../Shared/Stats.js"
 
-const Operator = ({opData, popTopOpListItem}) => {
+const Operator = ({opData, deleteListItem}) => {
 
     const {name} = opData;
 
@@ -18,7 +18,9 @@ const Operator = ({opData, popTopOpListItem}) => {
 
     const [onScreen, setOnScreen] = useState(true);
 
-    useEffect(() => {
+    const [justCreated, setJustCreated] = useState(true);
+
+    /*useEffect(() => {
         var observer = new IntersectionObserver(function(entries) {
             // isIntersecting is true when element and viewport are overlapping
             // isIntersecting is false when element and viewport don't overlap
@@ -29,10 +31,53 @@ const Operator = ({opData, popTopOpListItem}) => {
         }, { threshold: [.1]});
         
         observer.observe(document.getElementById("opListItem"));
-    }, [])
+    }, [])*/
+
+    useEffect(() => {
+
+        const opListItem = document.getElementById(opData.name);
+        /*console.log(opListItem)
+        console.log(opListItem.getBoundingClientRect())
+        if (opListItem.getBoundingClientRect().top < 0) {
+            console.log(opData.name)
+            console.log(opListItem.scrollTop)
+
+        }*/
+        document.addEventListener('scroll', () => {
+            const opListItem = document.getElementById(opData.name);
+            //console.log(opListItem.getBoundingClientRect())
+            if (opListItem.getBoundingClientRect().top < 0) {
+                deleteListItem(opData.name)
+            }
+        });
+
+        /*function isElementOutViewport(el){
+            var rect = el.getBoundingClientRect();
+            return rect.bottom < 0 || rect.right < 0 
+            || rect.left > window.innerWidth || rect.top > window.innerHeight;
+        }*/
+
+        /*var intersectionObserver = new IntersectionObserver(function(entries) {
+            // If intersectionRatio is 0, the target is out of view
+            // and we do not need to do anything.
+            if (entries[0].intersectionRect.top == 0) { 
+                console.log(entries);
+            };
+            console.log(opData.name)
+            console.log(entries)
+            if (entries[0].intersectionRect.top < 0) {
+                console.log('out' + opData.name);
+            }
+          
+          });
+          // start observing
+          intersectionObserver.observe(document.getElementById(opData.name));*/
+
+    }, []);
+
 
     return ( 
-        <div id="opListItem" className={opListItemClassName}>
+        <div id={opData.name} className={opListItemClassName}>
             <img 
                 className="opIconImg"
                 alt="operatorIcon"
--- src/components/OperatorList/OperatorList.js
+++ src/components/OperatorList/OperatorList.js
@@ -4,20 +4,26 @@ const OperatorList = ({operatorsData}) => {
 
     const operatorList = [];
 
-    const popTopOpListItem = () => {
+    this.deleteListItem = (opName) => {
         if (onScreenOpList.length === 1) return;
+        console.log("here")
         setOnScreenOpList(oldList => {
-            console.log(oldList.slice(1));
-            return oldList.slice(1);
+            const i = oldList.findIndex(op => op.name === opName);
+            if (i === 0) return oldList.slice(1);
+            else return oldList.slice(0, oldList.length - 1);
         });
-    }
+    };
 
     operatorsData.forEach((op) => {
         if (op.name !== "Oryx" && op.name !== "Melusi" && op.name !== "Iana" && op.name !== "Ace")
-        operatorList.push(<Operator key={op.name} opData={op} popTopOpListItem={popTopOpListItem} />);
+        operatorList.push(<Operator key={op.name} opData={op} deleteListItem={this.deleteListItem} />);
     });
     
     const [onScreenOpList, setOnScreenOpList] = useState(operatorList.splice(0, 1));
+
+    useEffect(() => {
+        this.deleteListItem = this.deleteListItem.bind(this);
+    }, []);
 
     useEffect(() => {
         document.addEventListener('scroll', () =>
